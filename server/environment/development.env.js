d7602e2ab24be6796a59c6bf5d1b84fda996e757
Pedro Narváez <pedromario@gmail.com>
2020-02-08 13:59:54 -0600

    pass

--- server/environment/development.env.js
+++ server/environment/development.env.js
@@ -1,5 +1,5 @@
 process.env.PORT = process.env.PORT || 3000;
-process.env.MONGODB_URL = process.env.MONGODB_URL || 'mongodb+srv://dbuser:bkw4sqKJ3NaU@cluster0-v4i5m.gcp.mongodb.net/mis-usuarios?retryWrites=true&w=majority';
+process.env.MONGODB_URL = process.env.MONGODB_URL || 'mongodb+srv://dbuser:bkw4aU@cluster0-v4i5m.gcp.mongodb.net/mis-usuarios?retryWrites=true&w=majority';
 process.env.SALT_ROUNDS = process.env.SALT_ROUNDS || 10;
 process.env.TOKEN_SEED = process.env.TOKEN_SEED || 'QWERTY12345';
 process.env.TOKEN_EXPIRES = process.env.TOKEN_EXPIRES || 6
