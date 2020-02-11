86b30cf7fecc9c224ec918479322bb101f1c186d
Pedro Narvaez <pedronarvaez@Pedros-MacBook-Pro.local>
2020-02-11 10:04:08 -0600

    DB URL

--- server/environment/development.env.js
+++ server/environment/development.env.js
@@ -2,4 +2,4 @@ process.env.PORT = process.env.PORT || 3000;
 process.env.MONGODB_URL = process.env.MONGODB_URL || 'mongodb://localhost:27017/mis-usuarios';
 process.env.SALT_ROUNDS = process.env.SALT_ROUNDS || 10;
 process.env.TOKEN_SEED = process.env.TOKEN_SEED || 'QWERTY12345';
-process.env.TOKEN_EXPIRES = process.env.TOKEN_EXPIRES || 6
+process.env.TOKEN_EXPIRES = process.env.TOKEN_EXPIRES || 60*60;
